6fe0c279bdf61df158bee4a32cb313fae0f5221f
cristianscaliaarrow <30871073+cristianscaliaarrow@users.noreply.github.com>
2017-08-09 13:35:19 -0300

    Structure
    Create basic Clases

---
+++ Assets/Scripts/ScenesId.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ids{
+	
+	public static void ShowHome(Rol rol){
+
+	}
+
+	public static void ShowRankin(Rol rol){
+
+	}
+
+	public static void ShowPremios(Rol rol){
+
+	}
+
+	public static void ShowBeerTender(Rol rol){
+
+	}
+
+	public static void ShowContacto(Rol rol){
+
+	}
+
+}
---
+++ Assets/Scripts/Structures.cs
@@ -0,0 +1,39 @@
+﻿public class User{
+	public string mail;
+	public string dni;
+	public string nombre;
+	public string apellido;
+	public Rol rol;
+	public string telefono;
+	public int idPM;
+}
+
+
+public class Local{
+	public string nombre;
+	public int idPM;
+	public int objetivoVolumen;
+	public int objetivoDisponibilidad;
+}
+
+public class Registros{
+	public string fecha;
+	public string email;
+	public string puntosMotivo;
+	public int cant;
+	public int puntos;
+
+}
+
+public class Premios{
+	public string nombre;
+	public string descripcion;
+	public UnityEngine.Sprite imagen;
+	public int puntos;
+	public int catalogoId;
+	public int sku;
+}
+
+public enum Rol{
+	STAFF,MANAGER,OWNER
+}
