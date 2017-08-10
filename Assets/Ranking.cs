dee36c92aa89bf8d6e14bbd8e64b1751a16466d8
cristianscaliaarrow <30871073+cristianscaliaarrow@users.noreply.github.com>
2017-08-10 15:59:02 -0300

    Ranking
    5 Rank Pos Dinamic
    With Out Server Connection

---
+++ Assets/Ranking.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ranking : MonoBehaviour {
+
+    public Image ico;
+    public Text rankName;
+
+    private void Awake()
+    {
+        ico = GameObject.Find(gameObject.name + "/Icon").GetComponent<Image>();
+        rankName = GameObject.Find(gameObject.name + "/RankName").GetComponent<Text>();
+    }
+
+    public void SetRankingIcon()
+    {
+
+    }
+
+}
---
+++ Assets/RankingCanvas.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RankingCanvas : MonoBehaviour {
+
+    public Ranking[] rank;
+
+    void Start () {
+        rank[0].rankName.text = "ASDD 0";
+	}
+	
+	void Update () {
+		
+	}
+}
