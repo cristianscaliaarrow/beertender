a34ba4c072bbe3ab3690edafe035d37f68fa32cd
cristianscaliaarrow <30871073+cristianscaliaarrow@users.noreply.github.com>
2017-08-10 17:28:15 -0300

    Contacto-Mensaje

---
+++ Assets/NavContato.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NavContato : MonoBehaviour {
+
+    public Image contacto;
+    public Image mensaje;
+
+    public float scaleOff;
+    public float scaleOn;
+
+    public Sprite contactoOn;
+    public Sprite contactoOff;
+
+    public Sprite mensajeOn;
+    public Sprite mensajeOff;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    public void BTN_Contacto()
+    {
+        contacto.sprite = contactoOn;
+        contacto.transform.localScale = Vector3.one* scaleOn;
+        mensaje.sprite = mensajeOff;
+        mensaje.transform.localScale = Vector3.one * scaleOff;
+    }
+
+    public void BTN_Mensaje()
+    {
+        contacto.sprite = contactoOff;
+        contacto.transform.localScale = Vector3.one * scaleOff;
+        mensaje.sprite = mensajeOn;
+        mensaje.transform.localScale = Vector3.one * scaleOn;
+
+    }
+}
