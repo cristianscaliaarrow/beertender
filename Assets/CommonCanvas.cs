c0eac276921dd900e57cb0ad3412f39c79bd57e2
cristianscaliaarrow <30871073+cristianscaliaarrow@users.noreply.github.com>
2017-08-10 13:48:58 -0300

    PerfilName
    Arreglos de Fonts Size

---
+++ Assets/CommonCanvas.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CommonCanvas : MonoBehaviour {
+
+    public Image icoPerfil;
+    public Text txtName;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
