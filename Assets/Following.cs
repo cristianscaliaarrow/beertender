249c02fe65543badc3a8f867cfe0629a41e73972
cristianscaliaarrow <30871073+cristianscaliaarrow@users.noreply.github.com>
2017-08-15 15:28:42 -0300

    Navegator Test GoogleAna

---
+++ Assets/Following.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Following : MonoBehaviour {
+    public GoogleAnalyticsV4 googleAnalytics;
+	// Use this for initialization
+	void Start () {
+        googleAnalytics.StartSession();
+        googleAnalytics.LogScreen("MAIN NAVEGATONR");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
